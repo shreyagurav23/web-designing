1b4433994f9011eca385b4d9dfa9d9cf1a62e626
shreyagurav23 <shreya.r.gurav23@gmail.com>
2025-02-27 14:55:09 +0530

    Update appfile.py

--- appfile.py
+++ appfile.py
@@ -8,7 +8,7 @@ st.title("code review and helper.")
 code=st.text_area("enter your python code")
 click=st.button("check the code.")
 if click:
- genai.configure(api_key=st.screts['GOOGLE_API_KEY']['GOOGLE_API_KEY']")
+ genai.configure(api_key=st.secrets['GOOGLE_API_KEY']['GOOGLE_API_KEY']")
 
 model = genai.GenerativeModel("gemini-1.5-flash")   
 response = model.generate_content(f"check if the python {code} code is correct and if it is not correct help to find the error and give correct code.")
